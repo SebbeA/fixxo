b3c9195818cc00acd6838243bf219da3b0763aa3
Sebastian Adolfsson <sadolfsson@hotmail.com>
2022-10-17 15:12:47 +0200

    new git, first commit with structure added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import HomeView from './views/HomeView';
+import ContactsView from './views/ContactsView';
+
+function App() {
+  return (
+    <>
+    <HomeView />
+    <ContactsView />
+    </>
+  );
+}
+
+export default App;
