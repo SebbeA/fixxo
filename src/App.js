0102f965e188c6c0b1b75565fb58cb6bc283930a
Sebastian Adolfsson <sadolfsson@hotmail.com>
2022-10-17 16:29:35 +0200

    browserroute added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomeView from './views/HomeView';
 import ContactsView from './views/ContactsView';
+import NotFoundView from './views/NotFoundView';
 
 function App() {
   return (
-    <>
-    <HomeView />
-    <ContactsView />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/contacts" element={<ContactsView />} />
+        <Route path="*" element={<NotFoundView />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
