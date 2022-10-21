ffc98da3e4be4f81851c5cb28fe1b207730495dd
Sebastian Adolfsson <sadolfsson@hotmail.com>
2022-10-21 09:40:58 +0200

    productcard, productgridsection, buttons, gridcardicon added

---
+++ src/components/ProductCard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const ProductCard = ({product}) => {
+
+    const addToWishList = (e) => {
+        console.log(`${e} 
+        added to wish list`)
+    }
+
+    const addToCompare = (e) => {
+        console.log("added to compare")
+    }
+
+    const addToCart = (e) => {
+        console.log("added to cart")
+    }
+
+  return (
+    <div className="col">
+        <div className="card">
+            <div className="card-img">
+                <div className="card-menu">
+                    <button onClick={addToWishList} className="card-icon"><i className="fa-regular fa-heart"></i></button>
+                    <button onClick={addToCompare} className="card-icon"><i className="fa-regular fa-code-compare"></i></button>
+                    <button onClick={addToCart} className="card-icon"><i className="fa-regular fa-bag-shopping"></i></button>
+                </div>
+                <NavLink to="/products/id" className="button-theme">Quick View</NavLink>
+            </div>
+            <div className="card-body">
+                <p className="card-category">{product.category}</p>
+                <h5 className="card-title">{product.name}</h5>
+                <p className="card-rating">
+                    <i className="fa-sharp fa-solid fa-star"></i>
+                    <i className="fa-sharp fa-solid fa-star"></i>
+                    <i className="fa-sharp fa-solid fa-star"></i>
+                    <i className="fa-sharp fa-solid fa-star"></i>
+                    <i className="fa-sharp fa-solid fa-star"></i>
+                </p>
+                <p className="card-price">{product.price}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductCard
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainMenuSection from '../sections/MainMenuSection'
 import FooterSection from '../sections/FooterSection'
+import ProductGridSection from '../sections/ProductGridSection'
 
 const HomeView = () => {
+
+  const [products, setProducts] = useState([
+    { id: 1, name: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5},
+    { id: 2, name: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5},
+    { id: 3, name: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5},
+    { id: 4, name: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5},
+    { id: 5, name: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5},
+    { id: 6, name: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5},
+    { id: 7, name: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5},
+    { id: 8, name: "Modern Black Blouse", category: "Fashion", price: "$35.00", rating: 5}
+  ])
+
   return (
     <>
       <header>
         <MainMenuSection />
       </header>
+      <ProductGridSection title="Featured Products" products={products} />
       <FooterSection />
     </>
   )
