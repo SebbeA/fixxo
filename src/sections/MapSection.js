133e64b54b55ea74a8f12a55c0d286510eb7807f
Sebastian Adolfsson <sadolfsson@hotmail.com>
2022-10-24 14:19:46 +0200

    breadcrumb and iframe added to contactpage

---
+++ src/sections/MapSection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MapSection = () => {
+  return (
+    <iframe 
+    className="google-map"
+    title="google-map"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23063.847313316805!2d15.201261998151232!3d59.27324330813351!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465c14e28e532c05%3A0xafa950e40634bfcb!2s%C3%96rebro%20Slott!5e0!3m2!1ssv!2sse!4v1666605328981!5m2!1ssv!2sse" 
+    allowFullScreen="" 
+    loading="lazy" 
+    referrerPolicy="no-referrer-when-downgrade">
+    </iframe>
+  )
+}
+
+export default MapSection
--- src/views/ContactsView.js
+++ src/views/ContactsView.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import MainMenuSection from '../sections/MainMenuSection'
 import FooterSection from '../sections/FooterSection'
+import BreadcrumbSection from '../sections/BreadcrumbSection'
+import MapSection from '../sections/MapSection'
 
 const ContactsView = () => {
   window.top.document.title = 'Contatcs | Fixxo.'
@@ -8,6 +10,8 @@ const ContactsView = () => {
   return (
     <>
       <MainMenuSection />
+      <BreadcrumbSection currentPage="Contatcs" />
+      <MapSection />
       <FooterSection />
     </>
   )
